b9d80517ddf7f764d78a3b131290005af2e7de64
Leandro Rey <leandrodrey@gmail.com>
2023-06-04 17:41:50 -0300

    Pages updated

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import {Inter} from 'next/font/google'
-import NavBar from "@/components/NavBar/NavBar";
 import MainLayout from "@/components/layouts/MainLayout";
-
-const inter = Inter({subsets: ['latin']})
 
 export default function About() {
     return (
