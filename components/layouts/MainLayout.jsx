9a6f6e2997662ba115d19eca247cee7dbaa2965d
Leandro Rey <leandrodrey@gmail.com>
2023-06-26 15:13:13 -0300

    Final of section number 2. NextJS Course

--- components/layouts/MainLayout.jsx
+++ components/layouts/MainLayout.jsx
@@ -1,26 +1,26 @@
 import Head from 'next/head';
-import { Navbar } from '../Navbar';
+import {Navbar} from '../Navbar';
 
 
 import styles from './MainLayout.module.css';
 
-export const MainLayout = ({ children }) => {
+export const MainLayout = ({children}) => {
     return (
         <div className={styles.container}>
-          
-          <Head>
-            <title>Home - Leandro Rey</title>
-            <meta name="description" content="Home Page" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-        
-          <Navbar />
-    
-          <main className={styles.main}>
-    
-            { children }
-    
-          </main>     
+
+            <Head>
+                <title>Home - Leandro Rey</title>
+                <meta name="description" content="Home Page"/>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <Navbar/>
+
+            <main className={styles.main}>
+
+                {children}
+
+            </main>
         </div>
-      )
+    )
 };
