44f7b78379be923d426f581595f4a010d415e931
Leandro Rey <leandrodrey@gmail.com>
2023-06-28 19:41:14 -0300

    Favorites

--- pokemon-static/pages/pokemon/[id].tsx
+++ pokemon-static/pages/pokemon/[id].tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {GetStaticPaths, GetStaticProps, NextPage} from "next";
 import {pokeApi} from "@/api";
 import {PokemonFull} from "@/interfaces";
 import {Layout} from "@/components/layouts";
 import {Button, Card, Container, Grid, Text} from "@nextui-org/react";
+import {localFavorites} from "@/utils";
 
 interface Props {
     pokemon: PokemonFull;
 }
 
 const PokemonPage: NextPage<Props> = (pokemon) => {
+
+    const [isInFavorites, setIsInFavorites] = useState(localFavorites.existsInFavorites(pokemon.pokemon.id));
+
+    const onToggleFavorite = () => {
+        localFavorites.toggleFavorite(pokemon.pokemon.id);
+        setIsInFavorites(!isInFavorites);
+    }
 
     return (
         <Layout>
@@ -32,9 +40,10 @@ const PokemonPage: NextPage<Props> = (pokemon) => {
                             <Text h1 transform='capitalize'>{pokemon.pokemon.name}</Text>
                             <Button
                                 color="gradient"
-                                ghost
+                                ghost={!isInFavorites}
+                                onPress={onToggleFavorite}
                             >
-                                Guardar en favoritos
+                                { isInFavorites ? 'Remove from favorites' : 'Add to favorites' }
                             </Button>
                         </Card.Header>
                         <Card.Body>
@@ -75,21 +84,21 @@ const PokemonPage: NextPage<Props> = (pokemon) => {
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
-    const pokemons151 = [...Array(151)].map( ( value, index ) => `${ index + 1 }` );
+    const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
 
     return {
-        paths: pokemons151.map( id => ({
-            params: { id }
+        paths: pokemons151.map(id => ({
+            params: {id}
         })),
         fallback: false
     }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({params}) => {
 
-    const { id } = params as { id: string };
+    const {id} = params as { id: string };
 
-    const { data } = await pokeApi.get<PokemonFull>(`/pokemon/${ id }`);
+    const {data} = await pokeApi.get<PokemonFull>(`/pokemon/${id}`);
 
     return {
         props: {
---
+++ pokemon-static/utils/localFavorites.ts
@@ -0,0 +1,24 @@
+const toggleFavorite = (id: number) => {
+    if (typeof window !== 'undefined') {
+        let favorites: number[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+        if (favorites.includes(id)) {
+            favorites = favorites.filter(pokeId => pokeId !== id);
+        } else {
+            favorites.push(id);
+        }
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+};
+
+const existsInFavorites = (id: number): boolean => {
+    if (typeof window !== 'undefined') {
+        const favorites: number[] = JSON.parse(localStorage.getItem('favorites') || '[]');
+        return favorites.includes(id);
+    }
+    return false;
+};
+
+export default {
+    toggleFavorite,
+    existsInFavorites
+};
