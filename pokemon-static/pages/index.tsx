011cc8fb6833f7db1b9a12f517e7cf5b3e574ba1
leandrodrey <leandrodrey@gmail.com>
2023-06-27 22:07:55 -0300

    Applying Next UI

--- pokemon-static/pages/index.tsx
+++ pokemon-static/pages/index.tsx
@@ -1,8 +1,10 @@
+import {Button} from "@nextui-org/react";
 
 export default function HomePage(): JSX.Element {
     return (
         <>
             <h1>Home Page</h1>
+            <Button color="gradient">Click me</Button>
         </>
     )
 }
