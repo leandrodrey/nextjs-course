cc5c1fd976f43b6ce69e3571ce85e8667ea5f9b4
Leandro Rey <leandrodrey@gmail.com>
2023-06-30 15:51:18 -0300

    Project 3. Reducer

--- open-jira/context/ui/uiReducer.ts
+++ open-jira/context/ui/uiReducer.ts
@@ -1,7 +1,22 @@
-export const uiReducer = () => {
-    return (
-        <>
-            Hola
-        </>
-    )
+import {UiState} from "@/context/ui/UiProvider";
+
+type UiActionType =
+    | { type: 'UI - Open Sidebar'; }
+    | { type: 'UI - Close Sidebar'; }
+
+export const uiReducer = (state: UiState, action: UiActionType): UiState => {
+    switch (action.type) {
+        case 'UI - Open Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: true,
+            }
+        case 'UI - Close Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: false,
+            }
+        default:
+            return state;
+    }
 }
