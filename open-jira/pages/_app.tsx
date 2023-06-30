360b03464765ffe64437b2ef9d819f60d1a451c0
Leandro Rey <leandrodrey@gmail.com>
2023-06-29 21:28:14 -0300

    Project 3 Start

--- open-jira/pages/_app.tsx
+++ open-jira/pages/_app.tsx
@@ -1,17 +1,24 @@
 import '@/styles/globals.css'
 import type {AppProps} from 'next/app'
-import CssBaseline from '@mui/material/CssBaseline';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import {ThemeProvider, createTheme} from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
 
-const basicTheme = createTheme();
+const basicTheme = createTheme(
+    {
+        palette: {
+            mode: 'dark',
+        }
+    }
+);
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider theme={basicTheme}>
+            <CssBaseline />
             <Component {...pageProps} />
         </ThemeProvider>
     )
