280b8a7169eecff924939b2c412dad1fc4c2b362
Leandro Rey <leandrodrey@gmail.com>
2023-07-14 18:43:42 -0300

    Project 4. Custom _document.tsx

--- cookies-master/pages/_document.tsx
+++ cookies-master/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, {DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript} from 'next/document'
+import {ReactElement} from "react";
 
 class MyDocument extends Document {
     static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -6,7 +7,7 @@ class MyDocument extends Document {
         return initialProps
     }
 
-    render() {
+    render(): ReactElement {
         return (
             <Html>
                 <Head/>
@@ -17,6 +18,7 @@ class MyDocument extends Document {
             </Html>
         )
     }
+
 }
 
 export default MyDocument
